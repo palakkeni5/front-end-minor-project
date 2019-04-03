c50815a169d835970f2a4d387962a84d7341727f
palakkeni5 <palakkeni@gmail.com>
2019-04-03 13:51:11 +0530

    give code to sagar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+              '../assets/css-styles/main_styles.css',
+              '../assets/css-styles/elements_responsive.css',
+              '../assets/css-styles/elements_styles.css',
+              '../assets/css-styles/responsive.css'
+            ]
 })
 export class AppComponent {
   title = 'angular-front-end';
